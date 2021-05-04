8e9b3bd8cadad77bddb50c8527c2d72e7ae0f067
Tim Van der Linden <tim.vanderlinden@uclouvain.be>
2021-05-04 13:24:56 +0200

    first test of minimacro5

---
+++ keyboards/minimacro5/keymaps/copypaste/config.h
@@ -0,0 +1,12 @@
+#define ENCODER_DIRECTION_FLIP
+
+#ifdef ENCODER_RESOLUTION
+#undef ENCODER_RESOLUTION
+#endif
+#define ENCODER_RESOLUTION 4
+
+                                
+
+
+
+                                
