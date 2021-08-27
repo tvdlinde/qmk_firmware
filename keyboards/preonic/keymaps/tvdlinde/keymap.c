142199af14e373371132e52b9b22b0f237c27875
Tim Van der Linden <tim.vanderlinden@uclouvain.be>
2021-08-27 20:30:09 +0200

    intermediate adapt

--- keyboards/preonic/keymaps/tvdlinde/keymap.c
+++ keyboards/preonic/keymaps/tvdlinde/keymap.c
@@ -85,9 +85,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_MAIN] = LAYOUT_ortho_5x12(  
        KC_1,           KC_2,           KC_3,          KC_4,         KC_5,            TD(DANCE_0),        KC_ESCAPE,          KC_6,             KC_7,           KC_8,         KC_9,          KC_0,  
        KC_Q,           KC_W,           KC_F,          KC_P,         KC_B,            KC_EQUAL,           KC_MINUS,           KC_J,             KC_L,           KC_U,         KC_Y,          KC_SCOLON, 
-       LT(_RSE,KC_A),  LT(_LWR,KC_R),  LGUI_T(KC_S),  LSFT_T(KC_T), KC_G,            TD(DANCE_4),        KC_QUOTE,           KC_M,             RSFT_T(KC_N),   RGUI_T(KC_E), KC_I,          LT(_RSE,KC_O),
+       LT(_RSE,KC_A),  LT(_LWR,KC_R),  LGUI_T(KC_S),  LSFT_T(KC_T), KC_G,            TD(DANCE_4),        KC_TAB,             KC_M,             RSFT_T(KC_N),   RGUI_T(KC_E), KC_I,          LT(_RSE,KC_O),
        KC_Z,           KC_X,           TD(DANCE_1),   KC_D,         KC_V,            KC_LBRACKET,        KC_RBRACKET,        KC_K,             KC_H,           KC_COMMA,     KC_DOT,        KC_SLASH, 
-       LALT_T(KC_F3),  OSM(MOD_LALT),  TG(_LWR),      TD(DANCE_2),  LGUI_T(KC_BSPC), LT(_MEH,KC_ENT),    LT(_MEH,KC_TAB),    LGUI_T(KC_SPACE), TD(DANCE_3),    TG(_RSE),     OSM(MOD_LCTL), OSM(MOD_LGUI)),
+       LALT_T(KC_F3),  OSM(MOD_LALT),  TG(_LWR),      TD(DANCE_2),  LGUI_T(KC_BSPC), LT(_MEH,KC_ENT),    LT(_MEH,KC_F12),    LGUI_T(KC_SPACE), TD(DANCE_3),    TG(_RSE),     OSM(MOD_LCTL), KC_QUOTE),
     [_RSE] = LAYOUT_ortho_5x12(
        KC_EXLM,        KC_AT,          KC_HASH,       KC_DLR,       KC_PERC,         LSFT(KC_GRAVE),     KC_TRNS,            KC_CIRC,          KC_AMPR,        KC_ASTR,      KC_LPRN,       KC_RPRN, 
        KC_TRNS,        KC_TRNS,        KC_LCBR,       KC_RCBR,      KC_TRNS,         KC_PLUS,            KC_UNDS,            KC_GRAVE,         KC_7,           KC_8,         KC_9,          KC_SCLN, 
