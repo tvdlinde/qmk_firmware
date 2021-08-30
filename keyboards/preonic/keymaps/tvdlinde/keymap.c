7f19da84fac0f9d73212bc109694dde33f6b0090
Tim Van der Linden <tim.vanderlinden@uclouvain.be>
2021-08-30 10:07:36 +0200

    delete

--- keyboards/preonic/keymaps/tvdlinde/keymap.c
+++ keyboards/preonic/keymaps/tvdlinde/keymap.c
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        MO(_MEH),       KC_TRNS,        KC_TRNS,       KC_TRNS,      LALT(KC_BSPC),   KC_TRNS,            KC_TRNS,            KC_TRNS,          KC_TRNS,        TO(_MAIN),    KC_TRNS,       KC_TRNS),
     [_MEH] = LAYOUT_ortho_5x12(
        MEH(KC_F1),     MEH(KC_F2),     MEH(KC_F3),    MEH(KC_F4),   MEH(KC_F5),      MEH(KC_F12),        MEH(KC_ESCAPE),     MEH(KC_F6),       MEH(KC_F7),     MEH(KC_F8),   MEH(KC_F9),    MEH(KC_F10),
-       KC_TRNS,        MEH(KC_W),      MEH(KC_F),     MEH(KC_P),    KC_TRNS,         KC_TRNS,            KC_TRNS,            KC_TRNS,          MEH(KC_L),      MEH(KC_U),    MEH(KC_Y),     KC_TRNS,
+       MEH(KC_DEL),    MEH(KC_W),      MEH(KC_F),     MEH(KC_P),    KC_TRNS,         KC_TRNS,            KC_TRNS,            KC_TRNS,          MEH(KC_L),      MEH(KC_U),    MEH(KC_Y),     KC_TRNS,
        MEH(KC_A),      MEH(KC_R),      MEH(KC_S),     LCA(KC_T),    MEH(KC_G),       MEH(KC_ENT),        MEH(KC_TAB),        MEH(KC_M),        MEH(KC_N),      MEH(KC_E),    LCA(KC_I),     MEH(KC_O),
        KC_TRNS,        KC_TRNS,        KC_TRNS,       KC_TRNS,      KC_TRNS,         MEH(KC_RGHT),       LCTL(KC_LEFT),      KC_TRNS,          KC_TRNS,        KC_TRNS,      KC_TRNS,       KC_TRNS, 
        KC_TRNS,        KC_TRNS,        KC_TRNS,       KC_TRNS,      MEH(KC_UP),      KC_TRNS,            KC_TRNS,            MEH(KC_DOWN),     KC_TRNS,        KC_TRNS,      KC_TRNS,       KC_TRNS),
