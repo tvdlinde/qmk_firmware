46dad0b57f0d7bf5cbe7d7ea3e3f255045c2d9a9
Tim Van der Linden <tim.vanderlinden@uclouvain.be>
2021-08-31 08:08:42 +0200

    meh esc on rotary

--- keyboards/preonic/keymaps/tvdlinde/keymap.c
+++ keyboards/preonic/keymaps/tvdlinde/keymap.c
@@ -89,13 +89,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,        KC_TRNS,        KC_TRNS,       KC_TRNS,      KC_TRNS,         KC_TRNS,            KC_TRNS,            KC_TRNS,          LCTL(KC_LEFT),  KC_UP,        LCTL(KC_RGHT), KC_TRNS, 
        KC_TRNS,        KC_TRNS,        LALT_T(KC_S),  KC_TRNS,      KC_TRNS,         KC_TRNS,            KC_TRNS,            LALT(KC_LEFT),    KC_LEFT,        KC_DOWN,      KC_RGHT,       LALT(KC_RGHT), 
        KC_TRNS,        KC_TRNS,        KC_TRNS,       KC_TRNS,      KC_TRNS,         KC_TRNS,            RESET,              KC_TRNS,          KC_BSPACE,      KC_PGDOWN,    KC_DELETE,     KC_TRNS, 
-       MO(_MEH),       KC_TRNS,        KC_TRNS,       KC_TRNS,      LALT(KC_BSPC),   KC_TRNS,            KC_TRNS,            KC_TRNS,          KC_TRNS,        TO(_MAIN),    KC_TRNS,       KC_TRNS),
+       KC_TRNS,        KC_TRNS,        KC_TRNS,       KC_TRNS,      LALT(KC_BSPC),   KC_TRNS,            KC_TRNS,            KC_TRNS,          KC_TRNS,        TO(_MAIN),    KC_TRNS,       KC_TRNS),
     [_MEH] = LAYOUT_ortho_5x12(
        MEH(KC_F1),     MEH(KC_F2),     MEH(KC_F3),    MEH(KC_F4),   MEH(KC_F5),      MEH(KC_RGHT),       LCTL(KC_LEFT),      MEH(KC_F6),       MEH(KC_F7),     MEH(KC_F8),   MEH(KC_F9),    MEH(KC_F10),
        MEH(KC_DEL),    MEH(KC_W),      MEH(KC_F),     MEH(KC_P),    KC_TRNS,         KC_TRNS,            KC_TRNS,            KC_TRNS,          MEH(KC_L),      MEH(KC_U),    MEH(KC_Y),     KC_TRNS,
-       MEH(KC_A),      MEH(KC_R),      MEH(KC_S),     LCA(KC_T),    MEH(KC_G),       MEH(KC_ESCAPE),     MEH(KC_TAB),        MEH(KC_M),        MEH(KC_N),      MEH(KC_E),    LCA(KC_I),     MEH(KC_O),
+       MEH(KC_A),      MEH(KC_R),      MEH(KC_S),     LCA(KC_T),    MEH(KC_G),       KC_TRNS,            MEH(KC_TAB),        MEH(KC_M),        MEH(KC_N),      MEH(KC_E),    LCA(KC_I),     MEH(KC_O),
        KC_TRNS,        KC_TRNS,        KC_TRNS,       KC_TRNS,      KC_TRNS,         KC_TRNS,            KC_TRNS,            KC_TRNS,          KC_TRNS,        KC_TRNS,      KC_TRNS,       KC_TRNS, 
-       KC_TRNS,        KC_TRNS,        KC_TRNS,       MEH(KC_UP),   MOD_LGUI,        MEH(KC_ENT),        MEH(KC_F12),        MOD_LGUI,         MEH(KC_DOWN),   KC_TRNS,      KC_TRNS,       KC_TRNS),
+       MEH(KC_ESCAPE), KC_TRNS,        KC_TRNS,       MEH(KC_UP),   MOD_LGUI,        MEH(KC_ENT),        MEH(KC_F12),        MOD_LGUI,         MEH(KC_DOWN),   KC_TRNS,      KC_TRNS,       KC_TRNS),
 };
 
 typedef struct {
