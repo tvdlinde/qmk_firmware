3de5978175901ca656bb19b33a0c5815e6980f77
Tim Van der Linden <tim.vanderlinden@uclouvain.be>
2021-10-17 01:35:45 +0200

    Backslash

--- keyboards/preonic/keymaps/tvdlinde/keymap.c
+++ keyboards/preonic/keymaps/tvdlinde/keymap.c
@@ -75,13 +75,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_MAIN] = LAYOUT_ortho_5x12(  
        KC_1,           KC_2,           KC_3,          KC_4,         KC_5,            KC_LBRACKET,        KC_RBRACKET,        KC_6,             KC_7,           KC_8,         KC_9,          KC_0,  
        KC_Q,           KC_W,           KC_F,          KC_P,         KC_B,            KC_EQUAL,           KC_MINUS,           KC_J,             KC_L,           KC_U,         KC_Y,          KC_SCOLON, 
-       LT(_RSE,KC_A),  LT(_LWR,KC_R),  LGUI_T(KC_S),  LSFT_T(KC_T), KC_G,            TD(DANCE_4),        TD(DANCE_0),        KC_M,             RSFT_T(KC_N),   RGUI_T(KC_E), KC_I,          LT(_RSE,KC_O),
+       LT(_RSE,KC_A),  LT(_LWR,KC_R),  LGUI_T(KC_S),  LSFT_T(KC_T), KC_G,            KC_BSLASH,          TD(DANCE_0),        KC_M,             RSFT_T(KC_N),   RGUI_T(KC_E), KC_I,          LT(_RSE,KC_O),
        KC_Z,           KC_X,           TD(DANCE_1),   KC_D,         KC_V,            LT(_MEH,KC_GRAVE),  LT(_MEH,KC_ESCAPE), KC_K,             KC_H,           KC_COMMA,     KC_DOT,        KC_SLASH, 
        LALT_T(KC_F3),  OSM(MOD_LALT),  TG(_LWR),      TD(DANCE_2),  LGUI_T(KC_BSPC), LT(_RSE,KC_ENT),    LT(_LWR,KC_F12),    LGUI_T(KC_SPACE), TD(DANCE_3),    TG(_RSE),     OSM(MOD_LCTL), KC_QUOTE),
     [_RSE] = LAYOUT_ortho_5x12(
        KC_F1,          KC_F2,          KC_F3,         KC_F4,        KC_F5,           LALT(KC_BSLASH),    LALT(KC_PIPE),      KC_F6,            KC_F7,          KC_F8,        KC_F9,         KC_F10, 
-       KC_TRNS,        KC_TRNS,        KC_LCBR,       KC_RCBR,      KC_TRNS,         KC_PPLS,            KC_UNDS,            KC_GRAVE,         KC_P7,          KC_P8,        KC_P9,         KC_SCLN, 
-       KC_TRNS,        KC_MINUS,       KC_LPRN,       KC_RPRN,      KC_DLR,          KC_TRNS,            LALT(KC_LPRN),      KC_MINUS,         KC_P4,          KC_P5,        KC_P6,         KC_TRNS, 
+       KC_TRNS,        KC_TRNS,        KC_LCBR,       KC_RCBR,      KC_TRNS,         KC_PPLS,            KC_UNDS,            KC_GRAVE,         KC_P7,          KC_P8,        KC_P9,         KC_BSLASH, 
+       KC_TRNS,        KC_MINUS,       KC_LPRN,       KC_RPRN,      KC_DLR,          LALT(LSFT(KC_9)),   LALT(KC_LPRN),      KC_MINUS,         KC_P4,          KC_P5,        KC_P6,         KC_TRNS, 
        KC_TRNS,        KC_TRNS,        KC_LBRC,       KC_RBRC,      KC_TRNS,         LSFT(KC_GRAVE),     KC_ESCAPE,          KC_TRNS,          KC_P1,          KC_P2,        KC_P3,         KC_TRNS, 
        KC_TRNS,        KC_TRNS,        TO(_MAIN),     KC_TRNS,      LALT(KC_BSPC),   KC_TRNS,            KC_TRNS,            KC_PENT,          KC_P0,          KC_TRNS,      KC_PDOT,       KC_TRNS),
     [_LWR] = LAYOUT_ortho_5x12(
