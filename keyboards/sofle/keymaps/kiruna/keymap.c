0ad95937d8780ed7d4037142367584d39520c05c
Tim Van der Linden <tim.vanderlinden@uclouvain.be>
2022-05-30 15:18:57 +0200

    Clean up layer code

--- keyboards/sofle/keymaps/kiruna/keymap.c
+++ keyboards/sofle/keymaps/kiruna/keymap.c
@@ -4,49 +4,8 @@
 
 #define HYP(kc) MT(MOD_LSFT | MOD_LALT | MOD_LCTL | MOD_LGUI, kc)
 
-
 #define INDICATOR_BRIGHTNESS 30
 
-#define HSV_OVERRIDE_HELP(h, s, v, Override) h, s , Override
-#define HSV_OVERRIDE(hsv, Override) HSV_OVERRIDE_HELP(hsv,Override)
-
-// Light combinations
-#define SET_INDICATORS(hsv) \
-    {0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
-    {35+0, 1, hsv}
-#define SET_UNDERGLOW(hsv) \
-    {1, 6, hsv}, \
-    {35+1, 6,hsv}
-#define SET_NUMPAD(hsv)     \
-    {35+15, 5, hsv},\
-      {35+22, 3, hsv},\
-      {35+27, 3, hsv}
-#define SET_NUMROW(hsv) \
-    {10, 2, hsv}, \
-        {20, 2, hsv}, \
-        {30, 2, hsv}, \
-      {35+ 10, 2, hsv}, \
-      {35+ 20, 2, hsv}, \
-      {35+ 30, 2, hsv}
-#define SET_INNER_COL(hsv)  \
-    {33, 4, hsv}, \
-      {35+ 33, 4, hsv}
-
-#define SET_OUTER_COL(hsv) \
-    {7, 4, hsv}, \
-      {35+ 7, 4, hsv}
-#define SET_THUMB_CLUSTER(hsv)  \
-    {25, 2, hsv}, \
-      {35+ 25, 2, hsv}
-#define SET_LAYER_ID(hsv)   \
-    {0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
-    {35+0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
-        {1, 6, hsv}, \
-    {35+1, 6, hsv}, \
-        {7, 4, hsv}, \
-      {35+ 7, 4, hsv}, \
-        {25, 2, hsv}, \
-      {35+ 25, 2, hsv}
 #define SET_ALL(hsv)  \
     {0, 70, hsv}
 
@@ -395,49 +354,22 @@ bool oled_task_user(void) {
 
 #ifdef RGBLIGHT_ENABLE
 char layer_state_str[70];
-// Now define the array of layers. Later layers take precedence
 
-// QWERTY,
-// Light on inner column and underglow
-const rgblight_segment_t PROGMEM layer_red_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-  SET_ALL(HSV_RED)
-
-);
 const rgblight_segment_t PROGMEM layer_main_lights[] = RGBLIGHT_LAYER_SEGMENTS(
   SET_ALL(HSV_BLACK)
 );
 
-// // _NUM,
-// // Light on outer column and underglow
-// const rgblight_segment_t PROGMEM layer_num_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-//     SET_LAYER_ID(HSV_TEAL)
-
-// );
-// _SYMBOL,
-// Light on inner column and underglow
 const rgblight_segment_t PROGMEM layer_blu_lights[] = RGBLIGHT_LAYER_SEGMENTS(
     SET_ALL(HSV_BLUE)
     );
-// _COMMAND,
-// Light on inner column and underglow
-// const rgblight_segment_t PROGMEM layer_command_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-//   SET_LAYER_ID(HSV_PURPLE)
-// );
 
-// //_NUMPAD
-// const rgblight_segment_t PROGMEM layer_numpad_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-//     SET_INDICATORS(HSV_ORANGE),
-//     SET_UNDERGLOW(HSV_ORANGE),
-//     SET_NUMPAD(HSV_BLUE),
-//     {7, 4, HSV_ORANGE},
-//     {25, 2, HSV_ORANGE},
-//     {35+6, 4, HSV_ORANGE},
-//     {35+25, 2, HSV_ORANGE}
-//     );
-// _SWITCHER   // light up top row
 const rgblight_segment_t PROGMEM layer_grn_lights[] = RGBLIGHT_LAYER_SEGMENTS(
     SET_ALL(HSV_GREEN)
 );
+
+const rgblight_segment_t PROGMEM layer_red_lights[] = RGBLIGHT_LAYER_SEGMENTS(
+  SET_ALL(HSV_RED)
+);  
 
 const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
 
@@ -457,6 +389,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
     return state;
 }
+
 void keyboard_post_init_user(void) {
     // Enable the LED layers
     rgblight_layers = my_rgb_layers;
