e6325af488fd14472da8c52e8ebd2c2701505855
Tim Van der Linden <tim.vanderlinden@pm.me>
2025-05-12 12:48:36 +0200

    Update left knob

--- keyboards/preonic/keymaps/kiruna/keymap.c
+++ keyboards/preonic/keymaps/kiruna/keymap.c
@@ -42,9 +42,9 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     break;
     default:
     if (clockwise) {
-        tap_code16(KC_MS_WH_DOWN);
+        tap_code(KC_VOLU);
     } else {
-        tap_code16(KC_MS_WH_UP);
+        tap_code(KC_VOLD);
     }
     break;
 }
